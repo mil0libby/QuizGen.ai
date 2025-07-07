8f1d7d0b49cece7c89914f997bc13f2067bbefbc
mil0libby <miloclibby@gmail.com>
2025-07-07 07:17:25 -0700

    Update socket.ts

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8080"); // Replace with your backend URL in production
+const socket = io("https://quizgensocket.onrender.com"); // Replace with your backend URL in production
+
 
 export default socket;
